8cbce52ed91a85f44f7815f5f7b57a6a5542785f
Leejaeho1234 <ljh2735294@naver.com>
2024-03-18 03:13:01 +0900

    프로젝트1 내용 수정

--- 15.js
+++ 15.js
@@ -12,6 +12,17 @@ gsap.to(sections, {
 		end: () => '+=' + document.querySelector('.horizontal').offsetWidth,
 	},
 });
+// gsap.to(sections, {
+// 	xPercent: -100 * (sections.length - 1),
+// 	ease: 'none',
+// 	scrollTrigger: {
+// 		trigger: '.horizontal2',
+// 		pin: true,
+// 		scrub: 1,
+// 		snap: 1 / (sections.length - 1),
+// 		end: () => '+=' + document.querySelector('.horizontal2').offsetWidth,
+// 	},
+// });
 
 //로티 로고
 const lottie = bodymovin.loadAnimation({
