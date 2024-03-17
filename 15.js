01c788bdbd418b38ebb3b6dc270a419ac4f2ef11
Leejaeho1234 <ljh2735294@naver.com>
2024-03-18 04:09:56 +0900

    프로젝트1 모바일 이미지 슬라이딩 추가

--- 15.js
+++ 15.js
@@ -34,10 +34,17 @@ gsap.to(sections, {
 // });
 
 //프로젝트1 사이트 사진 호버
-const p1FullPage = document.querySelector('.p1_full_page');
-p1FullPage.addEventListener('mouseenter', function(){
-	p1FullPage.style.backgroundPosition = 'center 100%';
+const pc1FullPage = document.querySelector('.pc1_full_page');
+const m1FullPage = document.querySelector('.m1_full_page');
+pc1FullPage.addEventListener('mouseenter', function(){
+	pc1FullPage.style.backgroundPosition = 'center 100%';
 });
-p1FullPage.addEventListener('mouseleave', function(){
-	p1FullPage.style.backgroundPosition = 'center 0%';
-})
+pc1FullPage.addEventListener('mouseleave', function(){
+	pc1FullPage.style.backgroundPosition = 'center 0%';
+});
+m1FullPage.addEventListener('mouseenter', function(){
+	m1FullPage.style.backgroundPosition = 'center 100%';
+});
+m1FullPage.addEventListener('mouseleave', function(){
+	m1FullPage.style.backgroundPosition = 'center 0%';
+});
